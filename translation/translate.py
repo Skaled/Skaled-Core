1641172a2cba6cc7735418a966c4e99a0af7c2a3
Skaled <112662417+Skaled@users.noreply.github.com>
2022-12-17 18:47:36 +0100

    Updated to v1.4.0

--- translation/translate.py
+++ translation/translate.py
@@ -1,11 +1,25 @@
 # Open this file with a text editor and translate the english phrases, in the qoutes, to your language.
 
+# System
+ts_system= "System"
+ts_blocker = "This section is not developed yet."
+ts_username = "Set a username in the settings before start to start to use the software"
+ts_username_changed = "Your username is now changed to: \n"
+ts_data_destroyed = "All the data has been deleted. \nRestart the software to create a new one."
+ts_return = "Return"
+ts_aborting = "Aborting. \nOne or more character can't be used."
+ts_is_blank = "Something is not allowed to be left blank.\nAborting."
+ts_update_manager = "You are not using the latest version available.\nDownload it to see the changes!"
+
+
+# Hub
 ts_changelog = "Changelog"
 ts_future_features = "Future Features"
 ts_settings = "Settings"
 ts_credits = "Credits"
 ts_license = "License"
 
+# Settings
 ts_change_username = "Change username: "
 ts_confirm_username = "Confirm username"
 ts_change_language = "Change language"
@@ -13,15 +27,24 @@ ts_confirm_language = "Confirm language"
 ts_destroy_data = "Destroy the data?"
 ts_confirm_destroy_data = "Confirm"
 
+# Skaled AI
 ts_send_message = "Send"
 ts_default_message = "Hello!"
 
-ts_available_services = "Available services: "
+# Skaled Manager
+# - window
+ts_file_gesture = "File Gesture:"
 ts_create_directory = "Create directory"
 ts_create_file = "Create file"
 ts_rename_directory = "Rename directory"
 ts_rename_file = "Rename file"
 
+ts_view_file = "View Files:"
+ts_view_photo = "View a Photo"
+ts_view_video = "View a Video"
+ts_view_zipped_file = "View a Zipped File"
+
+# - commands
 ts_select_path = "Select the path."
 ts_name_directory = "Enter the name of the directory:"
 ts_name_file = "Enter the name of the file:"
@@ -36,11 +59,3 @@ ts_create_rename_file = "Select the file you want to rename:"
 ts_create_name_file = "Enter the new name of the file:"
 ts_create_header_file = "Enter the new header of the file:"
 ts_create_rename_successful_file = "The file has been renamed."
-
-ts_system= "System"
-ts_blocker = "This section is not developed yet."
-ts_username = "Set a username in the settings before start to start to use the software"
-ts_username_changed = "Your username is now changed to: \n"
-ts_data_destroyed = "All the data has been deleted. \nRestart the software to create a new one."
-ts_return_ts = "Return"
-ts_aborting = "Aborting. \nOne or more character can't be used."
